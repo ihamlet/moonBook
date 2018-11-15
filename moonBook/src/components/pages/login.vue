6def6196e9ddd78f58f9020c75cce33bfe790b84
wandway <w2_001@qq.com>
2018-11-15 11:58:41 +0800

    Merge remote-tracking branch 'origin' into wandway
    # Conflicts:
    #	moonBook/src/router/index.js

--- moonBook/src/components/pages/login.vue
+++ moonBook/src/components/pages/login.vue
@@ -2,17 +2,15 @@
     <div>
         <div>登陆</div>
 
-        <form method="post">
+        <form method="post" @submit="onSubmit">
+            <van-cell-group>
+                <van-field type="tel" required v-model="user.mobile" placeholder="请输入手机号码" />
+            </van-cell-group>
+            <van-cell-group>
+                <van-field type="password" required v-model="user.password" placeholder="输入密码"></van-field>
+            </van-cell-group>
             <div>
-                <input type="text" name="account" v-model="user.mobile" placeholder="输入手机号码">
-            </div>
-
-            <div>
-                <input type="password" name="password" v-model="user.password" placeholder="输入密码">
-            </div>
-
-            <div>
-                <button>登陆</button>
+                <van-button type="primary">登陆</van-button>
             </div>
         </form>
     </div>
@@ -27,6 +25,28 @@
                     mobile:'',
                     password:''
                 }
+            }
+        },
+        methods:{
+            onSubmit(e){
+                e.preventDefault();
+                console.log('on submit', e);
+                let toast = this.$toast.loading({
+                   forbidClick:true,
+                   loadingType:'spinner'
+                });
+
+                this.$axios.post('/book/login/mobileLogin', this.user).then(res=>{
+                    console.log(res);
+                    toast.clear();
+                    if(res.data.status === 1) {
+                        console.log('登陆成功');
+                    } else {
+                        this.$toast.fail(res.data.info);
+                    }
+                });
+
+                return false;
             }
         }
     }
