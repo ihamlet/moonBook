a8e5de8978c30888c2b479ab1ffcb684ca4e382c
ihamlet <369774533@qq.com>
2018-11-16 13:27:02 +0800

    2018年11月16日13:27:01

---
+++ moonBook/src/components/module/search/searchCity.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="search">
+        <div class="search-bar">
+            <i class="iconfont">&#xe65c;</i>
+            {{placeholder}}
+        </div>
+    </div>    
+</template>
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+    name:'search',
+    data () {
+        return {
+            placeholder:'请搜索城市'
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<style scoped>
+.search{
+    padding: .3125rem /* 5/16 */ .625rem /* 10/16 */;
+    background: #ddd;
+}
+
+.search .search-bar{
+    text-align: left;
+}
+</style>
