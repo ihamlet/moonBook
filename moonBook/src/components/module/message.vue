ac8067bd34a848c2d480236e971aefb553390487
wandway <w2_001@qq.com>
2018-11-21 19:26:49 +0800

    消息更新

--- moonBook/src/components/module/message.vue
+++ moonBook/src/components/module/message.vue
@@ -47,211 +47,222 @@
     </div>
 </template>
 <script>
-import { mapActions } from 'vuex'
-import slogan from './../module/slogan'
-import axios from '@/fetch/api'
+import { mapActions, mapGetters } from "vuex";
+import slogan from "./../module/slogan";
+import axios from "@/fetch/api";
 
 export default {
-    name:'message',
-    props: ['readList'],
-    components: {
-        slogan
-    },
-    data () {
-        return {
-            loading: false,
-            finished: false,
-            tabDisabled:true,
-            currentTabIdx: 0,
-            tab:[{
-                title:'未读消息',
-                params:{
-                    page:1,
-                    limit:10,
-                    is_read:0,
-                    sort: 'top'
-                },
-                content: []
-            },{
-                title:'已读消息',
-                params:{
-                    page: 1,
-                    limit: 10,
-                    is_read: 1
-                },
-                content: []
-            }]
+  name: "message",
+  props: ["readList"],
+  components: {
+    slogan
+  },
+  data() {
+    return {
+      loading: false,
+      finished: false,
+      tabDisabled: true,
+      currentTabIdx: 0,
+      tab: [
+        {
+          title: "未读消息",
+          params: {
+            page: 1,
+            limit: 10,
+            is_read: 0,
+            sort: "top"
+          },
+          content: []
+        },
+        {
+          title: "已读消息",
+          params: {
+            page: 1,
+            limit: 10,
+            is_read: 1
+          },
+          content: []
         }
-    },
-    created () {
-        this.fetchData(this.tab[0]).then(this.fetchData(this.tab[1]));
-    },
-    watch: {
-        tab:{
-            handler(val){
-                if( val[1].content.length > 0 ){
-                    this.tabDisabled = false
-                }
-            },
-            deep:true
-        },
-        '$router':'fetchData'
-    },
-    methods: {
-        fetchData(tab){
-            const url = '/book/memberMsg/getList';
-            const params = {params:tab.params};
-            let $this = this;
-            return axios.get(url, params).then((res) => {
-                if(res.data.status === 1) {
-                    if(tab.params.page === 1) {
-                        tab.content = res.data.data;
-                        tab.count = res.data.count;
-                    } else {
-                        tab.content = tab.content.concat(res.data.data);
-                    }       
-                    $this.loading = false;           
-                    if(res.data.data.length == tab.params.limit)  {
-                        $this.finished = false;
-                        tab.params.page ++;
-                    } else {
-                        $this.finished = true;
-                    }                    
-                }                             
-            });
-        },
-        addRead(item){
-            const url = '/book/MemberMsg/addRead';
-            const params = {
-                params: {
-                    id:item.msg_id
-                }
-            };
-            let toast = this.$toast.loading({
-                forbidClick: true,
-                loadingType: 'spinner'
-            });
-            let $this = this;
-            axios.get(url, params).then(res=>{
-                toast.clear();
-                if(res.data.status === 1) {
-                    let content = $this.tab[0].content;
-                    let idx = content.indexOf(item);                
-                    if(idx !== -1) content.splice(idx, 1);
-                    $this.tab[0].count > 1 && $this.tab[0].count--;
-                } else {
-                    $this.$toast(res.data.msg);
-                }                
-            })
-        },
-        topping(item){
-            const url = '/book/MemberMsg/topping';
-            const params = {
-                params: {
-                    id: item.msg_id
-                }                
-            };
-            console.log('params', params);
-            let toast = this.$toast.loading({
-                forbidClick: true,
-                loadingType: 'spinner'
-            });
-            let $this = this;
-            axios.get(url, params).then(res=>{                
-                toast.clear();
-                if(res.data.status === 1) {
-                    $this.tab[0].params.page = 1;
-                    $this.fetchData($this.tab[0]);
-                } else {
-                    $this.$toast(res.data.msg);
-                }
-            })
-        },
-        onItemClick(item){
-            location.href = '/book/MemberMsg/detail?id=' + item.msg_id;
-        },
-        onTabClick(idx){
-            this.currentTabIdx = idx;
+      ]
+    };
+  },
+  created() {
+    this.fetchData(this.tab[0]).then(() => {
+      this.fetchData(this.tab[1]);
+    });
+  },
+  watch: {
+    tab: {
+      handler(val) {
+        if (val[1].content.length > 0) {
+          this.tabDisabled = false;
         }
+      },
+      deep: true
+    },
+    $router: "fetchData"
+  },
+  methods: {
+    ...mapActions(["getMsgs", "updateMsgLength"]),
+    async fetchData(tab) {
+      let msgs;
+      let delta = 0;
+
+      const params = { params: tab.params };
+      this.getMsgs(params).then(res => {
+        console.log('msgs', res);
+        if (params.params.page === 1) {
+          tab.content = res.data;
+          tab.count = res.count;
+        } else {
+          tab.content = tab.content.concat(res.data);
+        }
+
+        if (res.data.length == tab.params.limit) {
+          this.finished = false;
+          tab.params.page++;
+        } else {
+          this.finished = true;
+        }
+
+        this.loading = false;
+      });
+    },
+    addRead(item) {
+      const url = "/book/MemberMsg/addRead";
+      const params = {
+        params: {
+          id: item.msg_id
+        }
+      };
+      let toast = this.$toast.loading({
+        forbidClick: true,
+        loadingType: "spinner"
+      });
+      let $this = this;
+      axios.get(url, params).then(res => {
+        toast.clear();
+        if (res.data.status === 1) {
+          let msgLength = 0;
+          let tab = $this.tab[0];
+          let content = tab.content;
+          let idx = content.indexOf(item);
+          if (idx !== -1) content.splice(idx, 1);
+          msgLength = tab.count > 1 ? tab.count - 1: 0;
+          tab.count = msgLength;
+          $this.updateMsgLength(msgLength);
+        } else {
+          $this.$toast(res.data.msg);
+        }
+      });
+    },
+    topping(item) {
+      const url = "/book/MemberMsg/topping";
+      const params = {
+        params: {
+          id: item.msg_id
+        }
+      };
+      console.log("params", params);
+      let toast = this.$toast.loading({
+        forbidClick: true,
+        loadingType: "spinner"
+      });
+      let $this = this;
+      axios.get(url, params).then(res => {
+        toast.clear();
+        if (res.data.status === 1) {
+          $this.tab[0].params.page = 1;
+          $this.fetchData($this.tab[0]);
+        } else {
+          $this.$toast(res.data.msg);
+        }
+      });
+    },
+    onItemClick(item) {
+      location.href = "/book/MemberMsg/detail?id=" + item.msg_id;
+    },
+    onTabClick(idx) {
+      this.currentTabIdx = idx;
     }
-}
+  }
+};
 </script>
 <style scoped>
-.type{
-    font-size: 1rem /* 16/16 */;
-    color: #303133;
-    position: relative;
+.type {
+  font-size: 1rem /* 16/16 */;
+  color: #303133;
+  position: relative;
 }
 
-.text{
-    color: #606266;
+.text {
+  color: #606266;
 }
 
-.icon{
-    flex: 1;
-    width: 3.125rem /* 50/16 */;
-    height: 3.125rem /* 50/16 */;
-    line-height: 3.125rem /* 50/16 */;
-    text-align: center;
-    margin-right: .9375rem /* 15/16 */;
-    color: #fff;
-    border-radius: .375rem /* 6/16 */;
-    opacity: .8;
+.icon {
+  flex: 1;
+  width: 3.125rem /* 50/16 */;
+  height: 3.125rem /* 50/16 */;
+  line-height: 3.125rem /* 50/16 */;
+  text-align: center;
+  margin-right: 0.9375rem /* 15/16 */;
+  color: #fff;
+  border-radius: 0.375rem /* 6/16 */;
+  opacity: 0.8;
 }
 
-.icon.moon-book{
-    background: #FFC107;
+.icon.moon-book {
+  background: #ffc107;
 }
 
-.icon.system{
-    background: #03A9F4;
+.icon.system {
+  background: #03a9f4;
 }
 
-.msg-content{
-    flex: 6;
+.msg-content {
+  flex: 6;
 }
 
-.icon i.iconfont{
-    font-size: 1.75rem /* 28/16 */;
+.icon i.iconfont {
+  font-size: 1.75rem /* 28/16 */;
 }
 
-.tab-title{
-    position: relative;
+.tab-title {
+  position: relative;
 }
 
-.tab-title .unread{
-    position: absolute;
-    right: 2.5rem /* 40/16 */;
-    top: .3125rem /* 5/16 */;
+.tab-title .unread {
+  position: absolute;
+  right: 2.5rem /* 40/16 */;
+  top: 0.3125rem /* 5/16 */;
 }
 
-.date{
-    position: absolute;
-    top: 0;
-    right: 0;
-    color: #C0C4CC;
-    font-size: .75rem /* 12/16 */;
-    line-height: normal;
+.date {
+  position: absolute;
+  top: 0;
+  right: 0;
+  color: #c0c4cc;
+  font-size: 0.75rem /* 12/16 */;
+  line-height: normal;
 }
 
 .add-read,
-.topping{
-    width: 50%;
+.topping {
+  width: 50%;
 }
 
-.list .slot{
-    text-align: center;
-    height: 4.375rem /* 70/16 */;
-    line-height: 4.375rem /* 70/16 */;
-    color: #fff;
+.list .slot {
+  text-align: center;
+  height: 4.375rem /* 70/16 */;
+  line-height: 4.375rem /* 70/16 */;
+  color: #fff;
 }
 
-.add-read{
-    background: #C0C4CC;
+.add-read {
+  background: #c0c4cc;
 }
 
-.topping{
-    background: #03A9F4;
+.topping {
+  background: #03a9f4;
 }
-
 </style>
--- moonBook/src/store/index.js
+++ moonBook/src/store/index.js
@@ -7,88 +7,105 @@ import Cookies from 'js-cookie'
 Vue.use(Vuex)
 
 const state = {
-    slogan:'阅读点亮人生',
-    userData:{},
-    msgLength:1,
-    tabBtn:[],
-    amapApiKey:'0522f462288e296eac959dbde42718ab',
-    userPoint:''
+    slogan: '阅读点亮人生',
+    userData: {},
+    msgLength: 1,
+    tabBtn: [],
+    amapApiKey: '0522f462288e296eac959dbde42718ab',
+    userPoint: ''
 }
 
 const getters = {
-   userDataState: state=> {
-       return state.userData
-   },
-   MsgLengthState: state => {
-       return state.msgLength.length
-   },
-   dryingListLengthState: state =>{
-       if(state.userData.dryingList){
+    userDataState: state => {
+        return state.userData
+    },
+    MsgLengthState: state => {
+        return state.msgLength
+    },
+    dryingListLengthState: state => {
+        if (state.userData.dryingList) {
             return state.userData.dryingList.length
-       }
-   },
-   userPraiseState: state => {
-       let praise = []
-       if(state.userData.dryingList){
+        }
+    },
+    userPraiseState: state => {
+        let praise = []
+        if (state.userData.dryingList) {
             state.userData.dryingList.forEach(element => {
                 praise.push(element.social.praise.number)
-           })
-       }
-       return praise 
-   },
-   userPointState: state => {
-        if(state.userPoint){
+            })
+        }
+        return praise
+    },
+    userPointState: state => {
+        if (state.userPoint) {
             return state.userPoint
-        }else{
+        } else {
             let data
-            if(Cookies.get('userPoint')){
+            if (Cookies.get('userPoint')) {
                 data = JSON.parse(Cookies.get('userPoint'))
             }
             return state.userPoint = data
         }
-   },
-   userTabBtn: state =>{
-       return state.tabBtn
-   }
+    },
+    userTabBtn: state => {
+        return state.tabBtn
+    }
 }
 
 const mutations = {
-    setUserData(state, params){
+    setUserData(state, params) {
         state.userData = params.data
     },
-    setMsgLength(state,params){
-        state.msgLength = params.data
+    setMsgLength(state, count) {
+        state.msgLength = count
     },
-    setTabBtn(state,params){
+    setTabBtn(state, params) {
         state.tabBtn = params.data
     },
-    setUserPoint(state,params){
+    setUserPoint(state, params) {
         Cookies.set('userPoint', params.data, { expires: 1 })
         state.userPoint = params.data
+    },
+    getMsgs(state, msgs) {
+        state.msgs = msgs;
     }
 }
 
 const actions = {
-    getUserData(context){
-        axios.get('/book/memberUser/getInfo').then(res=>{
+    getUserData(context) {
+        axios.get('/book/memberUser/getInfo').then(res => {
             context.commit('setUserData', res.data)
         })
     },
-    getMsgLength(context){
-        axios.get('/api/messageList').then(res=>{
-            context.commit('setMsgLength',{
-                data:res.data.messageData.messageList
+    async getMsgLength(context) {
+        const url = '/book/MemberMsg/getList?is_read=0';
+        await axios.get(url).then((res) => {
+            if (res.data.status === 1) {
+                context.commit('setMsgLength', res.data.count);
+            }
+        });
+    },
+    async getMsgs(context, params = {}) {
+        const url = '/book/MemberMsg/getList';
+        return new Promise((resolve, reject)=>{
+            axios.get(url, params).then((res) => {
+                if (res.data.status === 1) {            
+                    resolve(res.data);
+                }
+            });
+        });
+    },
+    updateMsgLength(context, length) {
+        context.commit('setMsgLength', length);
+    },
+    getTabBtn(context) {
+        axios.get('/api/barBtn').then(res => {
+            context.commit('setTabBtn', {
+                data: res.data.barBtn
             })
         })
     },
-    getTabBtn(context){
-        axios.get('/api/barBtn').then(res=>{
-            context.commit('setTabBtn',{
-                data:res.data.barBtn
-            })
-        })
-    },
-    getUserLocation(context,products){
+    getUserLocation(context, products) {
         let cityInfo = {
             location: products.location
         }
@@ -105,33 +122,33 @@ const actions = {
         }).then(res => {
             cityInfo.city = res.regeocode.addressComponent.city
 
-            context.commit('setUserPoint',{
+            context.commit('setUserPoint', {
                 data: cityInfo
             })
         })
     },
-    getSchoolList(context,products){
+    getSchoolList(context, products) {
         let data = {
             Key: context.state.amapApiKey,
-            keywords:'教育',
+            keywords: '教育',
             types: 141204,
             location: products.location,
             offset: 20,
             page: products.page,
             radius: 24000
         }
-        
+
         let amapApiLink = `https://restapi.amap.com/v3/place/around?key=${data.Key}&location=${data.location}&radius=${data.radius}&keywords=${data.keywords}&types=${data.types}&offset=${data.offset}&page=${data.page}`
-        
+
         return new Promise((resolve, reject) => {
             fetchJsonp(amapApiLink).then(response => {
                 return response.json()
             }).then(res => {
                 resolve(res)
             })
-        }) 
+        })
     },
-    getSearch(context, products){
+    getSearch(context, products) {
         let data = {
             Key: context.state.amapApiKey,
             keywords: products.keywords,
@@ -139,25 +156,25 @@ const actions = {
             location: products.location,
             city: products.city,
             citylimit: true,
-            datatype:'all'
+            datatype: 'all'
         }
 
         let amamApiLink = `https://restapi.amap.com/v3/assistant/inputtips?key=${data.Key}&keywords=${data.keywords}&type=${data.type}&location=${data.location}&city=${data.city}&citylimit=${data.citylimit}&datatype=${data.datatype}`
-    
+
         return new Promise((resolve, reject) => {
             fetchJsonp(amamApiLink).then(response => {
                 return response.json()
             }).then(res => {
                 resolve(res)
             })
-        }) 
+        })
     },
-    getCityDistrict(context,products){
+    getCityDistrict(context, products) {
         let data = {
             Key: context.state.amapApiKey,
             keywords: products.city,
-            subdistrict:2,
-            extensions:'base'
+            subdistrict: 2,
+            extensions: 'base'
         }
 
         let amamApiLink = `https://restapi.amap.com/v3/config/district?key=${data.Key}&keywords=${data.keywords}&subdistrict=${data.subdistrict}&extensions=${data.extensions}`
@@ -170,12 +187,12 @@ const actions = {
                     city: res.districts[0].name,
                     location: res.districts[0].center
                 }
-                context.commit('setUserPoint',{
+                context.commit('setUserPoint', {
                     data: cityInfo
                 })
                 resolve(res)
             })
-        })     
+        })
     }
 }
 
@@ -185,4 +202,3 @@ export default new Vuex.Store({
     mutations,
     actions
 })
-  
