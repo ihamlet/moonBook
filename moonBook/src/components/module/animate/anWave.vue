3b847305ac532c30830d4b66f1678ca3db8a8b41
ihamlet <369774533@qq.com>
2018-11-22 10:58:13 +0800

    2018年11月22日10:58:11

--- moonBook/src/components/module/animate/anWave.vue
+++ moonBook/src/components/module/animate/anWave.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="container">
-        <div class="wave" />
+        <div class="wave">
+            <div class="raised-1"></div>
+            <div class="raised-2"></div>
+        </div>
     </div>
 </template>
 <script>
@@ -17,25 +20,26 @@ export default {
     left: 50%;
     transform: translate(-50%, 0);
     overflow: hidden;
+    user-select: none;
 }
 
 .wave {
     position: relative;
     width: 100%;
     height: 3.125rem /* 50/16 */;
-    background-color: transparent;
+    background: transparent;
     user-select: none;
 } 
 
-.wave::before,
-.wave::after{
+.raised-1,
+.raised-2{
     content: "";
     position: absolute;
     width: 100rem /* 1600/16 */;
     height: 100rem /* 1600/16 */;
     top: 2380%;
     left: 50%;
-    background-color: rgba(255, 255, 255, .4);
+    background: rgba(255, 255, 255, .4);
     border-radius: 45%;
     transform: translate(-50%, -70%) rotate(0);
     animation: rotate 6s linear infinite;
@@ -43,9 +47,9 @@ export default {
     user-select: none;
 }
 
-.wave::after{
+.raised-2{
     border-radius: 47%;
-    background-color: #fff;
+    background: #fff;
     transform: translate(-50%, -70%) rotate(0);
     animation: rotate 10s linear -5s infinite;
     z-index: 20;
