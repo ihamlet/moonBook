fecccb8bf1e47d5b9ba3d771da7234dc117c17a5
phin <369774533@qq.com>
2019-04-09 14:34:32 +0800

    推送

--- moonBook/src/components/pages/video/videoList.vue
+++ moonBook/src/components/pages/video/videoList.vue
@@ -1,44 +1,41 @@
 <template>
     <div class="video-list">
-        <van-swipe @change="onChange" :show-indicators='false' :loop="false">
+        <van-swipe @change="onChange" :show-indicators='false'>
             <van-swipe-item v-for='(item,index) in videoList' :key="index">
-                <video style="object-fit:fill" width="100%" height="100%" webkit-playsinline="true" x5-playsinline="" x5-video-player-fullscreen="" playsinline preload="auto"
-                           :poster="item.cover" :src="item.url" :playOrPause="playOrPause" x-webkit-airplay="true"
-                           x5-video-orientation="portrait" @click="pauseVideo" @ended="onPlayerEnded($event)"
-                           loop="loop"></video>
+                <div class="video-box">
+                    <div class="video-container" :class="`video-${index}`"></div>
+                </div>
             </van-swipe-item>
         </van-swipe>
-    
-        <div class="icon-play" v-show="iconPlayShow" @click="playvideo">
-            <i class="iconfont">&#xe639;</i>
-        </div>
     </div>
 </template>
 <script>
+import './../../../../static/ckplayer/ckplayer/ckplayer'
+
 export default {
     name:'video-list',
+    computed: {
+        player() {
+            let obj = this.videoObject
+            obj.poster = this.videoList[this.current].cover
+            obj.video = this.videoList[this.current].url
+            obj.container = `video-${this.current}`
+            return new ckplayer(obj)
+        }  
+    },
     data () {
-        let u = navigator.userAgent
         return {
-            // videoObject: {
-            //     container: '#video',
-            //     variable: 'player',
-            //     loaded: 'loadedHandler',
-            //     loop: false,
-            //     config: '',
-            //     debug: true,
-            //     drag: 'start',
-            //     seek: 0,
-            //     video: []
-            // }
-
+            videoObject: {
+                variable: 'player',
+                loaded: 'loadedHandler',
+                loop: false,
+                config: '',
+                debug: true,
+                drag: 'start',
+                seek: 0,
+                video: []
+            },
             current: 0,
-            isAndroid: u.indexOf('Android') > -1 || u.indexOf('Adr') > -1, //安卓终端
-            isIOS: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), // ios终端
-            isVideoShow: true,
-            playOrPause:true,
-            iconPlayShow: true,
-            video:null,
             videoList:[{
                 url:'https://activity-video.oss-cn-shanghai.aliyuncs.com/video/1',
                 cover:'https://activity-video.oss-cn-shanghai.aliyuncs.com/video/1?x-oss-process=video/snapshot,t_10000,m_fast'
@@ -57,46 +54,16 @@ export default {
             }]
         }
     },
+    mounted(){
+      this.$nextTick(()=>{
+          this.player.videoPlay()
+      })  
+    },
     methods: {
         onChange(index){
-            let video = document.querySelectorAll('video')[this.current]
-            video.play()
-            this.playOrPause = false
-            if(this.isIOS){
-                this.isVideoShow = false
-                this.pauseVideo()
-            }else{
-                this.isVideoShow = true
-                this.iconPlayShow = true
-            }
             this.current = index
-        },
-        playvideo(event) {
-            let video = document.querySelectorAll('video')[this.current]
-            console.log("playvideo：" + this.current)
-            this.isVideoShow = false
-            this.iconPlayShow = false
-            video.play()
-            window.onresize = function () {
-                video.style.width = window.innerWidth + "px"
-                video.style.height = window.innerHeight + "px"
-            }
-
-        },    
-        pauseVideo(){
-            let video = document.querySelectorAll('video')[this.current]
-            if (this.playOrPause) {
-                video.pause()
-                this.iconPlayShow = true
-            } else {
-                video.play()
-                this.iconPlayShow = false
-            }
-            this.playOrPause = !this.playOrPause
-        },
-        onPlayerEnded(player) { //视频结束
-            this.isVideoShow = true
-            this.current += this.current
+            console.log(this.player)
+            this.player.videoPlay()
         }
     }
 }
@@ -123,5 +90,10 @@ export default {
 .icon-play .iconfont{
     color: #fff;
     font-size: 30px;
+}
+
+.video-box,
+.video-container{
+    height: 100vh;
 }
 </style>
