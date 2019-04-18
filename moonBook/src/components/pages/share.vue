5039da34621975e43539ec9085486baae40ffbcd
phin <369774533@qq.com>
2019-04-18 17:46:29 +0800

    二维码链接

--- moonBook/src/components/pages/share.vue
+++ moonBook/src/components/pages/share.vue
@@ -94,7 +94,7 @@ export default {
       this.qrcode()
     },
     qrcode() {
-      let link = `${location.origin}/?#${qs.stringify(this.$route.query)}`
+      let link = `${location.href.replace('/#','/?#')}`
       QRcode.toDataURL(link).then(url => {
         this.codeImgURL = url
       }).catch(err => {
